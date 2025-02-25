b04d3ecc78a3cbf0cc8bb63f76e2463fdcd9a456
Varan <53681461+VTPrince@users.noreply.github.com>
2025-02-26 01:21:49 +0530

    Save user id and email using redux slice

--- src/slices/userInfoSlice.js
+++ src/slices/userInfoSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     user_id : 0,
     email : '',
+    isAuthenticated : false,
 }
 
 export const userInfoSlice = createSlice({
@@ -10,11 +11,18 @@ export const userInfoSlice = createSlice({
     initialState,
     reducers: {
         saveId : (state,action)=>{
-            state.user_id = action.payload
+            state.user_id = action.payload;
+            state.isAuthenticated = true
+            localStorage.setItem('userId',action.payload);
         },
         saveEmail : (state,action)=>{
             state.email = action.payload
         },
+        logout : (state)=>{
+            state.user_id = null;
+            state.isAuthenticated = false;
+            localStorage.removeItem('userId');
+        }
     },
 })
 
