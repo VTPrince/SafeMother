781795c39cb4c6ccd8a876a92f9f7363d0883e1f
Varan <53681461+VTPrince@users.noreply.github.com>
2025-03-17 17:25:54 +0530

    Made a common component to display card

---
+++ src/components/CardStat.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Card, CardContent, Typography, Box, Container, Grid2 } from "@mui/material";
+
+export const CardStat = ({title,value}) => {
+    return (
+        <>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h5" >
+                                {title}
+                            </Typography>
+                            <Typography>
+                                {value}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+        </>
+    )
+}
