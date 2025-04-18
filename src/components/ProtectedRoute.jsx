be66d8d69b320c13d03b866ae7029ee4c62075e9
Varan <53681461+VTPrince@users.noreply.github.com>
2025-04-19 02:14:56 +0530

    Refactor ProtectedRoute to include session check and loading state

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,9 +1,19 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
+
 const ProtectedRoute = ({element: Component, ...rest}) => {
-    const isAuthenticated = useSelector((state)=> state.userInfo.isAuthenticated);
+    const {isAuthenticated, isSessionChecked,user_id} = useSelector((state)=> ({
+        isAuthenticated : state.userInfo.isAuthenticated,
+        isSessionChecked : state.userInfo.isSessionChecked,
+        user_id : state.userInfo.user_id,
+    }));
+
+    if (!isSessionChecked) {
+        return <div>Loading...</div>;
+      }
+    
     return isAuthenticated ? <Component {...rest} /> : <Navigate to="/signin" />;
 };
 
